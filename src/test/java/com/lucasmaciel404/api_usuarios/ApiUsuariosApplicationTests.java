99dc0bca3ba8812804a438ec1bbe39859b8b70f0
Lucas Maciel <maciellucas487@gmail.com>
2025-08-09 11:31:52 -0300

    my first commit

---
+++ src/test/java/com/lucasmaciel404/api_usuarios/ApiUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmaciel404.api_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
